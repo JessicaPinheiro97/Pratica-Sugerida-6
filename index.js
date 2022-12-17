5ed5fd5b7e2aee0567df1216648db3d4354a2f27
Jéssica Pinheiro <109962238+JessicaPinheiro97@users.noreply.github.com>
2022-12-17 03:28:59 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+salario = parseFloat(prompt("Digite seu salário: "));
+ir = parseFloat(prompt("Digite seu imposto de renda: "));
+
+salario_liquido = salario - ir;
+
+console.log("Seu salário líquido é: R$ ", salario_liquido);
+
+// Exercício Aula 06 - Entrada de dados.
+
